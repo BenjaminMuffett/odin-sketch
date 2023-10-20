f57dcf45d212fc2917f276fc5af8aeee08de3596
BenjaminMuffett <136661576+BenjaminMuffett@users.noreply.github.com>
2023-10-20 11:36:59 +0900

    takes user input and refresh canvas

--- grid.js
+++ grid.js
@@ -1,32 +1,44 @@
 let sideLength = 16;
+const grid = document.querySelector("#grid");
+const btn = document.querySelector("#count");
 
-const grid = document.querySelector('#grid')
+generateCanvas(sideLength);
+
+btn.addEventListener("click", () => {
+  let userNumber = +prompt("Please enter a number between 1 ~ 100.");
+  if (userNumber > 100) {
+    userNumber = 100;
+  } else if (userNumber <= 0) {
+    userNumber = 1;
+  }
+  sideLength = userNumber;
+  grid.replaceChildren();
+  generateCanvas(sideLength);
+});
 
 // for (let i = 0; i < sideLength**2; i++) {
 //     const canvas = document.createElement('div')
 //     canvas.classList.add('canvas')
 //     grid.appendChild(canvas)
 // }
-
-for (let i = 0; i < sideLength; i++) {
-    const canvasLine = document.createElement('div')
-    canvasLine.classList.add('line')
+function generateCanvas(sideLength) {
+  for (let i = 0; i < sideLength; i++) {
+    const canvasLine = document.createElement("div");
+    canvasLine.classList.add("line");
     for (let z = 0; z < sideLength; z++) {
-        const canvas = document.createElement('div')
-        canvas.classList.add('block')
-        canvasLine.appendChild(canvas)
+      const canvas = document.createElement("div");
+      canvas.classList.add("block");
+      canvasLine.appendChild(canvas);
     }
-    grid.appendChild(canvasLine)
+    grid.appendChild(canvasLine);
+  }
+  const trail = document.querySelectorAll(".block");
+  trail.forEach((block) => {
+    block.addEventListener("mouseover", () => {
+      block.style.background = "black";
+    });
+  });
 }
-
-const trail = document.querySelectorAll('.block')
-trail.forEach((block) => {
-   block.addEventListener('mouseover', () => {
-    block.style.background = 'black'}) 
-});
-
-
-
 
 // trail.addEventListener('mouseover', (event) => {
 //     event.target.style.color = 'black'
